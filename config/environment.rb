b763dc77c1e04f739f0b8688c090cc6fe34c9ca5
Klinton Grider <klintgrider@gmail.com>
2019-04-10 23:54:26 +0000

    Updated mailer settings in environment file

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,12 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-Actionmailer::Base.smtp_settings = {
-    :port           => EVN['MAILGUN_SMTP_PORT']
-    :address        => EVN['MAILGUN_SMTP_SERVER']
-    :user_name      => EVN['MAILGUN_SMTP_LOGIN']
-    :password       => EVN['MAILGUN_SMTP_PASSWORD']
-    :domain         => 'shrouded-dusk-75859.herokuapp.com',
-    :authentication => :plain,
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'shrouded-dusk-75859.herokuapp.com',
+  :authentication => :plain,
 }
-Actionmailer::Base.delivery_method = :smtp
+ActionMailer::Base.delivery_method = :smtp
